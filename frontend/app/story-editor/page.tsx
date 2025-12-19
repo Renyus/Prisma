aa02ee6397aad0eb3f0d1c74605e34b54941f7e4
Renyus <1367309651@qq.com>
2025-12-19 10:10:02 +0800

    删除冗余文件，准备后端重构

--- frontend/app/story-editor/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-  return <div className="p-6">剧情编辑器 页面占位</div>;
-}
--- frontend/app/toolbox/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-  return <div className="p-6">工具箱 页面占位</div>;
-}
